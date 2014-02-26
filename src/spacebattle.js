e05edb457a52ec71725c1f80e99bcb6f9322cc03
EuPhobos <euphobos@gmail.com>
2014-02-26 12:16:33 +0400

    Now my ship can cum :)))

--- src/spacebattle.js
+++ src/spacebattle.js
@@ -1,11 +1,21 @@
+
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var H=0; var W=0;
+var background = new Image();
+background.src = "data/bg.jpg";
 var stars = [];
+var _key = 0;
+var fps = 0;
+var fps_limit = 30; //not working for now
+var bullets_max = 1000;
 
-function setSize(){
-	W = window.innerWidth - 20;
-	H = window.innerHeight - 20;
+function setSize(w,h){
+	if ( typeof h === "undefined" ) H = window.innerHeight - 20;
+	else H = h;
+	if ( typeof h === "undefined" ) W = window.innerWidth - 20;
+	else W = w;
+	
 	ctx.canvas.width  = W;
 	ctx.canvas.height = H;
 	
@@ -15,6 +25,18 @@ function setSize(){
 	}
 }
 setSize();
+
+
+window.requestAnimFrame = (function(){
+	return  window.requestAnimationFrame	|| 
+		window.webkitRequestAnimationFrame	|| 
+		window.mozRequestAnimationFrame		|| 
+		window.oRequestAnimationFrame		|| 
+		window.msRequestAnimationFrame		|| 
+		function(callback, element){
+			window.setTimeout(callback, 1000 / 60);
+		};
+})();
 
 
 window.onresize = setSize;
@@ -62,10 +84,10 @@ function create_star()
 	this.vx = Math.random()*-.3-.1;
 	this.vy = 0;
 	
-	var wcol = Math.random()*50+20>>0;
-	var r = wcol+Math.random()*10>>0;
-	var g = wcol+Math.random()*10>>0;;
-	var b = wcol+Math.random()*10>>0;
+	var wcol = Math.random()*150+50>>0;
+	var r = wcol+Math.random()*50>>0;
+	var g = wcol+Math.random()*50>>0;;
+	var b = wcol+Math.random()*50>>0;
 	this.color = "rgb("+r+", "+g+", "+b+")";
 	
 	this.radius = Math.random()*2+1;
@@ -74,13 +96,12 @@ function create_star()
 
 function _cos(a){return Math.cos(a*Math.atan(1)/45);}
 function _sin(a){return Math.sin(a*Math.atan(1)/45);}
-
-function draw()
-{
-
+var tick = 0;
+function draw(){
+	ctx.drawImage(background,0,0);
 //	ctx.globalCompositeOperation = "source-over";
 //	ctx.fillStyle = "rgba(0, 0, 0, 0.003)";
-	ctx.fillRect(0, 0, W, H);
+//	ctx.fillRect(0, 0, W, H);
 //	ctx.globalCompositeOperation = "lighter";
 
 	for(var t = 0; t < stars.length; t++)
@@ -105,6 +126,8 @@ function draw()
 		if(p.x > W+0) p.x = -0;
 		if(p.y > H+0) p.y = -0;
 	}
+
+	draw_bullet();
 
 	// draw ship
 	ctx.fillStyle   = '#ccc';
@@ -146,7 +169,7 @@ function draw()
 	ctx.textAlign = "right";
 	ctx.textBaseline = "bottom";
 	ctx.fillStyle = "red";
-	ctx.fillText('x:'+sh.vx.toFixed(2)+', y:'+sh.vy.toFixed(2),W-10,H-10 );
+	ctx.fillText('bullets:'+Bullet.all.length+'; fps:'+fps+';  key:'+_key+';  x:'+sh.vx.toFixed(2)+', y:'+sh.vy.toFixed(2),W-10,H-10 );
 
 	ctx.fillStyle = "#000";
 
@@ -166,20 +189,102 @@ function draw()
 	if (keys[39]) sh.a+=3;		// right
 	//if (keys[40])		// down
 
+	tick++;
 }
 //draw();
-setInterval(draw, 33);
+//setInterval(draw, 20);
 
+
+
+
+
+
+function getFPS(){
+	fps = tick;
+	tick = 0;
+}
+setInterval(getFPS,1000);
 
 var keys=[];
 window.addEventListener("keydown", function (e) {
     keys[e.keyCode] = true;
+
+	if ( e.keyCode == '77' ) setSize (800, 600);
+	if ( e.keyCode == '72' ) setSize ();
+	_key = e.keyCode;
 //	alert(e.keyCode);
 });
 window.addEventListener("keyup", function (e) {
     keys[e.keyCode] = false;
 });
 
+
+function draw_bullet(){
+	if (Bullet.all.length < bullets_max) {
+		if (keys[32]){
+		for (var i = 0; i < 5; i++) {
+			var bullet = new Bullet(sh.x, sh.y, 2);
+			var random_offset = Math.random() * .1 - .5;
+			var speed = Math.random() * 15 + 3;
+			bullet.vx = speed * _cos(sh.a + random_offset);
+			bullet.vy = speed * _sin(sh.a + random_offset);
+		}}
+	}
+
+	var i = Bullet.all.length;
+	while(i--) {
+		var bullet = Bullet.all[i];
+		bullet.x += bullet.vx;
+		bullet.y += bullet.vy;
+		bullet.vy += .1;
+		bullet.vx *= .999;
+		bullet.vy *= .99;
+		if (bullet.x > W || bullet.x < 0) {
+			bullet.remove();
+		}
+//		else if (bullet.y >= canvas.height) {
+		if (bullet.y > H){
+//			bullet.vy = -Math.abs(bullet.vy);
+//			bullet.vy *= .7;
+//			if (Math.abs(bullet.vy) < 1 && Math.abs(bullet.vx) < 1) {
+			bullet.remove();
+//			}
+		}
+	}
+	Bullet.draw_all();
+}
+
+function Bullet(x, y, r) {
+	this.x = x;
+	this.y = y;
+	this.r = r;
+	this.vx = 0;
+	this.vy = 0;
+	Bullet.all.push(this);
+}
+Bullet.all = [];
+Bullet.draw_all = function(){
+	var i = Bullet.all.length;
+	while (i--) {
+		Bullet.all[i].draw();
+	}
+};
+
+Bullet.prototype = {
+	draw: function() {
+		ctx.save();
+		ctx.translate(this.x, this.y);
+		ctx.fillStyle = "#ccc";
+		ctx.beginPath();
+		ctx.arc(0, 0, this.r, 0, Math.PI * 2, true);
+		ctx.closePath();
+		ctx.fill();
+		ctx.restore();
+	},
+	remove: function() {
+		Bullet.all.splice(Bullet.all.indexOf(this), 1);
+	}
+};
 
 /*
 $(document).keydown(function(e){
@@ -202,7 +307,10 @@ $(document).keydown(function(e){
 
 
 
-
+(function animloop(){
+  requestAnimFrame(animloop);
+  draw();
+})();
 
 
 
